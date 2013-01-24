b3479dab801c90cc697414a915879d10c5481227
karl <karl+github@la-grange.net>
2013-01-24 14:21:02 -0500

    the script get the list of all blog posts

---
+++ myoperabkp.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2.7
+# encoding: utf-8
+"""
+myoperabkp.py
+
+Created by Karl Dubost on 2013-01-24.
+Copyright (c) 2013 Grange. All rights reserved.
+see LICENSE.TXT
+"""
+
+import argparse
+import sys
+import requests
+import logging
+from lxml import etree
+from urlparse import urljoin
+# import os
+# import locale
+
+# variables
+myopath = "http://my.opera.com/%s/archive/"
+
+
+def getcontent(uri):
+    """Given a uri, parse an html document"""
+    headers = {'User-Agent': "MyOpera-Backup/1.0"}
+    r = requests.get(uri, headers=headers)
+    responsetext = r.text
+    logging.info("parsed %s" % (uri))
+    return responsetext
+
+
+def archivenext(content):
+    pass
+
+
+def blogpostlist(useruri):
+    "return a list of blog posts URI for a given username"
+    postlist = []
+    myparser = etree.HTMLParser(encoding="utf-8")
+    archivehtml = getcontent(useruri)
+    tree = etree.HTML(archivehtml, parser=myparser)
+    navlinks = tree.xpath('//p[@class="pagenav"]//a/@href')
+    # Insert the first page of the archive at the beginning
+    navlinks.insert(0, useruri)
+    # Remove the last item of the list which is the next link
+    navlinks.pop()
+    # we go through all the list
+    for navlink in navlinks:
+        archtml = getcontent(urljoin(useruri, navlink))
+        tree = etree.HTML(archtml, parser=myparser)
+        links = tree.xpath('//div[@id="arc"]//li//a/@href')
+        for link in links:
+            postlist.append(urljoin(useruri, link))
+    return postlist
+
+
+def main():
+    logging.basicConfig(filename='log-myopera.txt',
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    # Parsing the cli
+    parser = argparse.ArgumentParser(
+        description="Export html content from my.opera")
+    parser.add_argument(
+        '-u',
+        action='store',
+        dest="username",
+        help='username we want to backup')
+
+    args = parser.parse_args()
+    username = args.username
+    useruri = myopath % (username)
+    print len(blogpostlist(useruri))
+
+if __name__ == "__main__":
+    sys.exit(main())
